415465849fbf625046ad117e4842b388a4373a46
ifotn <rich@infrontofthenet.com>
2018-03-06 09:43:53 -0500

    crud with mlab lesson 7

--- config/globals.js
+++ config/globals.js
@@ -1,4 +1,5 @@
 // public application level variables
 module.exports = {
-  'db': 'mongodb://localhost:27017/cartracker'
+  //'db': 'mongodb://localhost:27017/cartracker'
+    'db': 'mongodb://carsdba:Pankasbmw@ds157818.mlab.com:57818/cartracker'
 };
--- controllers/cars.js
+++ controllers/cars.js
@@ -44,5 +44,62 @@ router.post('/add', (req, res, next) => {
    }) ;
 });
 
+// GET: /cars/delete/abc123
+router.get('/delete/:_id', (req, res, next) => {
+    // get the _id parameter from the url and store in a local variable
+    let _id = req.params._id;
+
+    // use the Car model to delete the document with this id
+    Car.remove({ _id: _id }, (err) => {
+       if (err) {
+           console.log(err);
+       }
+       else {
+           res.redirect('/cars');
+       }
+    });
+});
+
+// GET: /cars/edit/abc123
+router.get('/edit/:_id', (req, res, next) => {
+   // get _id param from url
+   let _id = req.params._id;
+
+   // use the Car model to find the selected document
+    Car.findById(_id, (err, car) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.render('cars/edit', {
+                title: 'Car Details',
+                car: car
+            });
+        }
+    });
+});
+
+// POST: /cars/edit/abc123
+router.post('/edit/:_id', (req, res, next) => {
+   // get the _id from the url
+   let _id = req.params._id;
+
+   // use the Mongoose update method to set all the new values
+   Car.update({ _id: _id },
+       { $set: {
+           make: req.body.make,
+               model: req.body.model,
+               year: req.body.year,
+               colour: req.body.colour
+           }}, null, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.redirect('/cars');
+        }
+       });
+});
+
 // make public
 module.exports = router;
